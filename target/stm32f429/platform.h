6a11948a6ff79684f26a3dc528a97d99edbb5c27
Jim Huang <jserv.tw@gmail.com>
2017-08-11 23:49:27 +0800

    Clarify stm32-specific identifier

--- target/stm32f429/platform.h
+++ target/stm32f429/platform.h
@@ -1,9 +1,9 @@
-#ifndef _PLATFORM_LM3S6965_PLATFORM_H
-#define _PLATFORM_LM3S6965_PLATFORM_H
+#ifndef _PLATFORM_STM32_PLATFORM_H
+#define _PLATFORM_STM32_PLATFORM_H
 
 #include <cmsis.h>
 
 void __platform_init(void);
 void __platform_halt(void);
 
-#endif /* !_PLATFORM_LM3S6965_PLATFORM_H */
+#endif /* !_PLATFORM_STM32_PLATFORM_H */
