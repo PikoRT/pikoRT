89032da006379175ade7775347e5d273d3d18479
Jim Huang <jserv.tw@gmail.com>
2017-08-11 23:45:04 +0800

    Use C99 designated initializers

--- target/stm32f429/init.c
+++ target/stm32f429/init.c
@@ -3,7 +3,7 @@
 #include "platform.h"
 
 #define CPU_FREQ_IN_HZ 168000000
-#define SYSTICK_FREQ_IN_HZ  1000
+#define SYSTICK_FREQ_IN_HZ 1000
 #define SYSTICK_PERIOD_IN_MSECS (SYSTICK_FREQ_IN_HZ / 1000)
 
 struct timer_operations;
@@ -37,44 +37,41 @@ void __printk_init(void)
 
     /* Enable GPIO TX/RX clock */
     __HAL_RCC_GPIOA_CLK_ENABLE();
+
     /* Enable USART1 clock */
     __HAL_RCC_USART1_CLK_ENABLE();
 
-    /* GPIO init */
-    GPIO_InitTypeDef GPIO_InitStruct;
-
-    /* Configure peripheral GPIO*/
-
-    /* UART TX/RX GPIO pin configuration  */
-    GPIO_InitStruct.Pin = GPIO_PIN_9 | GPIO_PIN_10;
-    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FAST;
-    GPIO_InitStruct.Alternate = GPIO_AF7_USART1;
+    /* GPIO initialization with UART Tx/Rx configuration */
+    GPIO_InitTypeDef GPIO_InitStruct = {
+        .Pin = GPIO_PIN_9 | GPIO_PIN_10,
+        .Mode = GPIO_MODE_AF_PP,
+        .Pull = GPIO_NOPULL,
+        .Speed = GPIO_SPEED_FAST,
+        .Alternate = GPIO_AF7_USART1,
+    };
 
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-    /* USART init */
-    UART_HandleTypeDef UartHandle;
-
-    /* Configure the UART peripheral */
-
-    /* Put the USART peripheral in the Asynchronous mode (UART Mode) */
+    /* Set the USART peripheral in the Asynchronous mode (UART Mode) */
     /* UART1 configured as follow:
         - Word Length = 8 Bits
         - Stop Bit = One Stop bit
         - Parity = None
         - BaudRate = 9600 baud
         - Hardware flow control disabled (RTS and CTS signals) */
-    UartHandle.Instance = USART1;
-
-    UartHandle.Init.BaudRate = 115200;
-    UartHandle.Init.WordLength = UART_WORDLENGTH_8B;
-    UartHandle.Init.StopBits = UART_STOPBITS_1;
-    UartHandle.Init.Parity = UART_PARITY_NONE;
-    UartHandle.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-    UartHandle.Init.Mode = UART_MODE_TX_RX | UART_IT_RXNE;
-    UartHandle.Init.OverSampling = UART_OVERSAMPLING_16;
+    UART_HandleTypeDef UartHandle = {
+        .Instance = USART1,
+        .Init =
+            {
+                .BaudRate = 115200,
+                .WordLength = UART_WORDLENGTH_8B,
+                .StopBits = UART_STOPBITS_1,
+                .Parity = UART_PARITY_NONE,
+                .HwFlowCtl = UART_HWCONTROL_NONE,
+                .Mode = UART_MODE_TX_RX | UART_IT_RXNE,
+                .OverSampling = UART_OVERSAMPLING_16,
+            },
+    };
 
     HAL_UART_Init(&UartHandle);
 
