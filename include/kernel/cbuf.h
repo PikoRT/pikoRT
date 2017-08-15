fd6194dc5f86284aac1c28c022f23220f24d4570
Jim Huang <jserv.tw@gmail.com>
2017-08-15 08:45:28 +0800

    Reimplement circular buffer as inline functions

--- include/kernel/cbuf.h
+++ include/kernel/cbuf.h
@@ -10,8 +10,30 @@ struct cbuf_info {
     void *buf;
 };
 
-void cbuf_init(struct cbuf_info *cbuf, void *buf, size_t len);
-int cbuf_getc(struct cbuf_info *cbuf, char *c);
-int cbuf_putc(struct cbuf_info *cbuf, char c);
+static inline void cbuf_init(struct cbuf_info *cbuf, void *buf, size_t len)
+{
+    cbuf->len = len;
+    cbuf->pos_begin = 0;
+    cbuf->pos_end = 0;
+    cbuf->buf = buf;
+}
+
+static inline int cbuf_getc(struct cbuf_info *cbuf, char *c)
+{
+    if (cbuf->pos_begin == cbuf->pos_end)
+        return 0;
+    *c = *((char *) cbuf->buf + cbuf->pos_begin);
+    cbuf->pos_begin = (cbuf->pos_begin + 1) % cbuf->len;
+
+    return 1;
+}
+
+static inline int cbuf_putc(struct cbuf_info *cbuf, char c)
+{
+    *((char *) cbuf->buf + cbuf->pos_end) = c;
+    cbuf->pos_end = (cbuf->pos_end + 1) % cbuf->len;
+
+    return 0;
+}
 
 #endif /* !_KERNEL_CBUF_H */
