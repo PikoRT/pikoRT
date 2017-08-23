e41244d5e07cc6263fda857ea014c9debda4bc31
Louie Lu <git@louie.lu>
2017-08-23 18:45:13 +0800

    Fix cmsis util build when missing

--- mk/cmsis.mk
+++ mk/cmsis.mk
@@ -11,9 +11,11 @@ $(CMSIS)/arm:
 $(CMSIS)/TARGET_STM:
 	svn export -r$(SVN_REV) --force https://github.com/ARMmbed/mbed-os/trunk/targets/TARGET_STM/ $(CMSIS)/TARGET_STM
 
-$(CMSIS)/util:
-	@mkdir -p $(CMSIS)/util
-# Reference: https://stackoverflow.com/questions/1125476/retrieve-a-single-file-from-a-repository
-#            https://stackoverflow.com/questions/1078524/how-to-specify-the-location-with-wget
-	wget https://raw.github.com/ARMmbed/mbed-os/master/platform/mbed_preprocessor.h -P $(CMSIS)/util
-	wget https://raw.github.com/ARMmbed/mbed-os/master/platform/mbed_assert.h -P $(CMSIS)/util
+ARM_CMSIS_UTIL = \
+	'mbed_preprocessor.h' \
+	'mbed_assert.h'
+
+$(CMSIS)/util: $(ARM_CMSIS_UTIL)
+$(ARM_CMSIS_UTIL):
+	$(VECHO) "  WGET\t\t$@\n"
+	$(Q)$(WGET) -q https://raw.github.com/ARMmbed/mbed-os/master/platform/$@ -P $(CMSIS)/util
